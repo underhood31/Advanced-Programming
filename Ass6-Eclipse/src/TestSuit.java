c5f8bddf9e90770dd4022720f41010e0d31b4f75
underhood31 <manavjeet18295@iiitd.ac.in>
2019-09-30 22:29:50 +0530

    Added Junit suite

---
+++ Ass6-Eclipse/src/TestSuit.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+//@RunWith(JUnitPlatform.class)
+
+@Suite.SuiteClasses({
+	ExceptionTest.class,
+	SerializationTest.class
+
+})
+public class TestSuit {
+	
+}
